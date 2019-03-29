59d12d6ff6e32bc944eb99778d340d720455b32f
szerintedmi <szerintedmi@gmail.com>
2019-03-29 16:28:10 +0000

    proper testing of tx confirmations

--- test/fails.js
+++ test/fails.js
@@ -7,36 +7,66 @@ const providers = baseHelpers.providers;
 
 providers.forEach(web3 => {
     describe("tx fails - " + web3.currentProvider.constructor.name, () => {
-        it("should resolve whith VM revert error", async () => {
-            const transactionHashSpy = sinon.spy();
-            const confirmationSpy = sinon.spy();
-            const receiptSpy = sinon.spy();
-            const onErrorSpy = sinon.spy();
-            const catchErrorSpy = sinon.spy();
+        it.only("should resolve when VM revert error", async () => {
+            // fails on beta36 and beta51 - confirmations or receipt events never triggered
+            //      receipt is not available neither in resolved tx or in error event args
+            return new Promise(async resolve => {
+                const transactionHashSpy = sinon.spy();
+                const confirmationSpy = sinon.spy();
+                const receiptSpy = sinon.spy();
+                const onErrorSpy = sinon.spy();
+                const catchErrorSpy = sinon.spy();
 
-            const accounts = await web3.eth.personal.getAccounts();
-            const dummyContract = await baseHelpers.getWeb3ContractInstance(
-                web3,
-                "../../build/contracts/DummyContract.json"
-            );
+                const accounts = await web3.eth.personal.getAccounts();
+                const dummyContract = await baseHelpers.getWeb3ContractInstance(
+                    web3,
+                    "../../build/contracts/DummyContract.json"
+                );
 
-            const receipt = dummyContract.methods
-                .revertMe()
-                .send({ from: accounts[0] })
-                .on("transactionHash", transactionHashSpy)
-                .on("confirmation", confirmationSpy)
-                .on("receipt", receiptSpy)
-                .on("error", onErrorSpy);
+                // testing:
+                // 1. transactionHash and receipt events are triggered once and before any confirmation event
+                // 2. confirmations event triggered  TRANSACTION_CONFIRMATION_BLOCKS times
+                // 3. tx resolved before confirmation event triggered on the TRANSACTION_CONFIRMATION_BLOCKS time
+                // 4. error event triggered before confirmation event
+                // 5. error thrown before confirmation event
+                let resolved = false;
+                const receipt = await dummyContract.methods
+                    .revertMe()
+                    .send({ from: accounts[0] })
+                    .on("transactionHash", transactionHashSpy)
+                    .on("receipt", receiptSpy)
+                    .on("confirmation", (confirmationNumber, receipt) => {
+                        console.log("con");
+                        confirmationSpy(confirmationNumber, receipt);
+                        assert(receipt);
+                        assert(!receipt.status);
+                        sinon.assert.calledOnce(transactionHashSpy);
+                        sinon.assert.calledOnce(receiptSpy);
+                        sinon.assert.calledOnce(onErrorSpy);
+                        sinon.assert.calledOnce(catchErrorSpy);
+                        if (confirmationNumber === baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS) {
+                            assert(resolved);
+                            resolve();
+                        }
+                    })
+                    .on("error", (error, receipt) => {
+                        assert(error);
+                        assert(receipt);
+                        assert(!receipt.status);
+                        onErrorSpy(error, receipt);
+                    })
+                    .catch(catchErrorSpy);
 
-            await receipt.catch(catchErrorSpy);
+                // It's never resolves with receipt but should it?
+                // assert(receipt);
+                // assert(!receipt.status);
+                sinon.assert.calledOnce(onErrorSpy);
+                sinon.assert.calledOnce(catchErrorSpy);
 
-            assert(!receipt.status);
-            sinon.assert.calledOnce(onErrorSpy);
-            sinon.assert.calledOnce(catchErrorSpy);
-
-            sinon.assert.calledOnce(transactionHashSpy);
-            sinon.assert.calledOnce(receiptSpy);
-            sinon.assert.callCount(confirmationSpy, baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS);
-        });
+                sinon.assert.calledOnce(transactionHashSpy);
+                sinon.assert.calledOnce(receiptSpy);
+                resolved = true;
+            });
+        }).timeout(baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS * 1000 + 2000);
     });
 });
--- test/sendTransaction.js
+++ test/sendTransaction.js
@@ -8,25 +8,40 @@ const providers = baseHelpers.providers;
 providers.forEach(web3 => {
     describe("sendTransaction - " + web3.currentProvider.constructor.name, () => {
         it("should trigger tx events after send", async () => {
-            // Failing with beta 51
-            assert.equal(web3.transactionConfirmationBlocks, baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS);
-            const accounts = await web3.eth.personal.getAccounts();
+            // Works with beta36 HttpProvider but failing with WebsocketProvider(no confirmations received).
+            // Failing both with beta 51 (sendTransaction never resolves. confirmations are still not received with WebsocketProvider)
+            return new Promise(async resolve => {
+                const accounts = await web3.eth.personal.getAccounts();
 
-            const transactionHashSpy = sinon.spy();
-            const confirmationSpy = sinon.spy();
-            const receiptSpy = sinon.spy();
+                const transactionHashSpy = sinon.spy();
+                const confirmationSpy = sinon.spy();
+                const receiptSpy = sinon.spy();
 
-            const receipt = await web3.eth
-                .sendTransaction({ to: accounts[1], from: accounts[0], value: web3.utils.toWei("0.1", "ether") })
-                .on("transactionHash", transactionHashSpy)
-                .on("confirmation", confirmationSpy)
-                .on("receipt", receiptSpy);
+                // testing:
+                // 1. transactionHash and receipt events are triggered once and before any confirmation event
+                // 2. confirmations event triggered  TRANSACTION_CONFIRMATION_BLOCKS times
+                // 3. tx resolved before confirmation event triggered on the TRANSACTION_CONFIRMATION_BLOCKS time
+                let resolved = false;
+                const receipt = await web3.eth
+                    .sendTransaction({ to: accounts[1], from: accounts[0], value: web3.utils.toWei("0.1", "ether") })
+                    .on("transactionHash", transactionHashSpy)
+                    .on("receipt", receiptSpy)
+                    .on("confirmation", (confirmationNumber, receipt) => {
+                        confirmationSpy(confirmationNumber, receipt);
+                        assert(receipt.status);
+                        sinon.assert.calledOnce(transactionHashSpy);
+                        sinon.assert.calledOnce(receiptSpy);
+                        if (confirmationNumber === baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS) {
+                            assert(resolved);
+                            resolve();
+                        }
+                    });
 
-            assert(receipt.status);
-
-            sinon.assert.calledOnce(transactionHashSpy);
-            sinon.assert.calledOnce(receiptSpy);
-            sinon.assert.callCount(confirmationSpy, baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS);
-        });
+                assert(receipt.status);
+                sinon.assert.calledOnce(transactionHashSpy);
+                sinon.assert.calledOnce(receiptSpy);
+                resolved = true;
+            });
+        }).timeout(baseHelpers.TRANSACTION_CONFIRMATION_BLOCKS * 1000 + 2000);
     });
 });
