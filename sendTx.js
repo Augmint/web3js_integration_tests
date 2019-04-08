b76c3e85cd8be1f92c8714e4df557b3b318025a3
szerintedmi <szerintedmi@gmail.com>
2019-04-08 11:30:48 +0100

    transactionConfirmationBlocks to 1 + comments

--- sendTx.js
+++ sendTx.js
@@ -2,12 +2,13 @@ const Web3 = require("web3");
 
 const OPTIONS = {
   defaultBlock: "latest",
-  transactionConfirmationBlocks: 3,
+  transactionConfirmationBlocks: 1,
   transactionBlockTimeout: 5
 };
 
 const web3 = new Web3(
   new Web3.providers.WebsocketProvider("ws://localhost:8545"),
+  // Web3.providers.HtttpProvider("http://localhost:8545")
   null,
   OPTIONS
 );
@@ -38,14 +39,13 @@ async function sendTx() {
       value: web3.utils.toWei("0.1", "ether")
     })
     .on("transactionHash", txHash => {
-      //web3.currentProvider.send("evm_mine");
+      //web3.currentProvider.send("evm_mine"); // execution of it seems to be blocked by sendTransaction in beta52
       console.log("on transactionHash", txHash);
     })
     .on("receipt", receipt => {
       console.log("on receipt");
     })
     .on("confirmation", (confirmationNumber, receipt) => {
-      // ganache.advanceBlock(web3); // execution of it seems to be blocked by sendTransaction in beta52
       console.log("on confirmation", confirmationNumber);
     })
     .on("error", error => {
